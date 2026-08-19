dbbd83babaaa47561ca8d121a99cf1b9f8606e17
Felix Wang <319334854@qq.com>
2026-08-19 20:28:29 +0800

    出包

--- SDK/apps/common/third_party_profile/rdx_protocol/rdx_app_config.h
+++ SDK/apps/common/third_party_profile/rdx_protocol/rdx_app_config.h
@@ -673,8 +673,8 @@
 #define WIFI_AP_SSID                           "Beanstalk RKB"
 #define WIFI_AP_PASSWORD                       "88888888"
 
-#define FIRMWARE_VERSION                       "1.0.0"
-#define FIRMWARE_VERSION_HEX                   0x00010000
+#define FIRMWARE_VERSION                       "1.0.1"
+#define FIRMWARE_VERSION_HEX                   0x00010001
 #define HARDWARE_VERSION                       "0.0.1"
 #define HARDWARE_VERSION_HEX                   0x00000001
 
