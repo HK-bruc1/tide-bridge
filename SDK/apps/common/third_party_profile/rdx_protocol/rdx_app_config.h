0114058b531017c34a8136b5182d61f3ee0b13a4
Felix Wang <319334854@qq.com>
2026-09-22 19:08:57 +0800

    出包

--- SDK/apps/common/third_party_profile/rdx_protocol/rdx_app_config.h
+++ SDK/apps/common/third_party_profile/rdx_protocol/rdx_app_config.h
@@ -697,8 +697,8 @@
 #define WIFI_AP_SSID                           "GensparkVoicePad"
 #define WIFI_AP_PASSWORD                       "88888888"
 
-#define FIRMWARE_VERSION                       "1.0.5"
-#define FIRMWARE_VERSION_HEX                   0x00010005
+#define FIRMWARE_VERSION                       "1.0.6"
+#define FIRMWARE_VERSION_HEX                   0x00010006
 #define HARDWARE_VERSION                       "0.0.1"
 #define HARDWARE_VERSION_HEX                   0x00000001
 
